792194b8a1d28cc3a83d471ca674ed72fc8a8341
Gabriel Stellini <stellini@ecbm.me>
2023-08-26 17:30:31 +0200

    Add scss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<p class="text-teal-500">Tailwind is working</p>
+<h1 class="text-teal-500">Tailwind is working</h1>
+<h1 class="text-teal-500 red">Override works</h1>
+<h1 class="myTeal">Inline reuse of existing classes works </h1>
 
+<style lang="scss">
+	.red {
+		color: tomato;
+	}
+
+  .myTeal {
+    @apply text-teal-500;
+  }
+</style>
