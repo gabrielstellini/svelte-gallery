ee848143594bdbbfe1e50d736017fda0394aa626
Gabriel Stellini <stellini@ecbm.me>
2023-08-27 20:14:45 +0200

    Add svelisy demo, add theming, add IDE folders

--- src/routes/daisy-ui/+page.svelte
+++ src/routes/daisy-ui/+page.svelte
@@ -1,1 +1,9 @@
-<button class="btn">Hello daisyUI</button>
+<script lang="ts">
+	import { Button } from 'svelisy';
+</script>
+
+<!--Native daisy elements-->
+<button class="btn btn-lg btn-primary">Test</button>
+
+<!--svelisy library element-->
+<Button class="btn-lg btn-primary">Test</Button>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,23 @@ const daisyui = require('daisyui');
 /** @type {import('tailwindcss').Config}*/
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					"primary": "#641ae6",
+					"secondary": "#d926a9",
+					"accent": "#1fb2a6",
+					"neutral": "#2a323c",
+					"base-100": "#1d232a",
+					"info": "#3abff8",
+					"success": "#36d399",
+					"warning": "#fbbd23",
+					"error": "#f87272",
+				},
+			},
+		],
+	},
 	theme: {
 		extend: {}
 	},
